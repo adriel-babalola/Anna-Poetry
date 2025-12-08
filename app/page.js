84cc931d370708634bf2addd8c07cef3aab42768
adriel-babalola <babalolaaauge115@st.futminna.edu.ng>
2025-12-08 23:40:31 +0100

    front end working

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 'use client'
-import BlogItem from "@/components/BlogItem";
+import BlogList from "@/components/BlogList";
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 export default function Home() {
   return (
     <div>
-     <Header />
-     <BlogItem />
+      <Header />
+      <BlogList />
+      <Footer />
     </div>
   );
 }
